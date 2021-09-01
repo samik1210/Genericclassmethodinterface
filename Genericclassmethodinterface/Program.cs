992ef8054445f0e23db074e8265df0c5cfe9c67a
parag <parag@DESKTOP-I2Q9QKQ>
2021-09-01 13:18:29 +0530

    Add project files.

---
+++ Genericclassmethodinterface/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Genericclassmethodinterface
+{
+    public class MyGenericArray<T>
+    {
+        private T[] array;
+
+        public MyGenericArray(int size)
+        {
+            array = new T[size + 1];
+        }
+        public T getItem(int index)
+        {
+            return array[index];
+        }
+        public void setItem(int index, T value)
+        {
+            array[index] = value;
+        }
+    }
+    class Tester
+    {
+        static void Main(string[] args)
+        {
+            MyGenericArray<int> intArray = new MyGenericArray<int>(5);
+            for (int c = 0; c < 5; c++)
+            {
+                intArray.setItem(c, c * 5);
+            }
+            for (int c = 0; c < 5; c++)
+            {
+                Console.Write(intArray.getItem(c) + " ");
+            }
+
+            Console.WriteLine();
+            MyGenericArray<char> charArray = new MyGenericArray<char>(5);
+            for (int c = 0; c < 5; c++)
+            {
+                charArray.setItem(c, (char)(c + 97));
+            }
+            for (int c = 0; c < 5; c++)
+            {
+                Console.Write(charArray.getItem(c) + " ");
+            }
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
